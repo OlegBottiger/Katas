ea21b2f30a5084b0afdecf2bdfcea60547b9075d
Oleg Lindvin <oleg.lindvin@gmail.com>
2018-06-22 15:03:21 +0200

    SOLVES Kata

--- Mumbling/index.js
+++ Mumbling/index.js
@@ -1,3 +1,13 @@
 module.exports = (text) => {
-  return 'C-Ww-Aaa-Tttt';
+  const textArray = [...text];
+  let result = '';
+
+  for(const [index, value ] of textArray.entries()) {
+    for(let i = 0; i < index + 1; i++) {
+      i === 0 ? result += value.toUpperCase() : result += value.toLowerCase();
+    }
+    index !== textArray.length - 1 ? result += '-' : null;
+  }
+
+  return result;
 }
